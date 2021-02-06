19ea5a26d9ab1a2887d52a7b2402702dc7ad13ea
yuya-yuya <yytyfp5115@icloud.com>
2021-02-06 16:42:21 +0000

    additional commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,6 +11,12 @@ class PhotosController < ApplicationController
     redirect_to photos_path
   end
   
+  def destroy
+     photo = Photo.find(params[:id])
+     photo.destroy
+     redirect_to photos_path
+  end
+  
   private
   
   def photo_params
